a0d6644a041338ad56c8d6699ac8df331aac0385
Sagar <sagar@Vins-MacBook-Air.local>
2025-10-08 17:51:42 +0530

    Adds token bucket rate limiting algorithm

--- LowLevelDesign/java/rate-limiter/src/com/ratelimiter/RateLimiterDemo.java
+++ LowLevelDesign/java/rate-limiter/src/com/ratelimiter/RateLimiterDemo.java
@@ -2,6 +2,7 @@ package com.ratelimiter;
 
 import com.ratelimiter.strategies.FixedWindowRateLimiter;
 import com.ratelimiter.strategies.RateLimitingStrategy;
+import com.ratelimiter.strategies.TokenBucketRateLimiter;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -13,6 +14,9 @@ public class RateLimiterDemo {
 
         System.out.println("----Fixed Window Demo----");
         runFixedWindowDemo(userId);
+
+        System.out.println("----Token Bucket Demo----");
+        runTokenBucketDemo(userId);
     }
 
     public static void runFixedWindowDemo(String userId) {
@@ -25,6 +29,26 @@ public class RateLimiterDemo {
 
         ExecutorService executor = Executors.newFixedThreadPool(3);
         for(int i=0; i<10;i++) {
+            executor.submit(() -> service.handleRequest(userId));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        executor.shutdown();
+    }
+
+    private static void runTokenBucketDemo(String userId) {
+        int capacity = 5;
+        int refillRate = 1; // 1 token per second
+
+        RateLimitingStrategy tokenBucketStrategy = new TokenBucketRateLimiter(capacity, refillRate);
+        RateLimiterApplication service = RateLimiterApplication.getInstance();
+        service.setRateLimitingStrategy(tokenBucketStrategy);
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        for(int i = 0; i<10;i++) {
             executor.submit(() -> service.handleRequest(userId));
             try {
                 Thread.sleep(500);
